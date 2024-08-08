3c82a0960d7fb47cfe7fc5852d07ab613a0ad712
Emmanuel Mensah <emomensa@gmail.com>
2024-08-08 14:10:56 +0000

    chore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { authInterceptor } from './interceptor/auth.interceptor';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
-    declarations: [AppComponent, LoginPageComponent, RegisterPageComponent],
+    declarations: [AppComponent, LoginPageComponent, RegisterPageComponent, HomePageComponent, LayoutComponent],
     imports: [BrowserModule, AppRoutingModule, MatIconModule, ReactiveFormsModule, MatFormFieldModule],
-    providers: [provideAnimationsAsync()],
+    providers: [provideAnimationsAsync(), provideHttpClient(withInterceptors([authInterceptor]))],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
