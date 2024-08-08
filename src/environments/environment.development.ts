9d8978b4f0a595c175818687bd2692a57360a3e3
Emmanuel Mensah <emomensa@gmail.com>
2024-08-08 14:32:11 +0000

    feat: remove env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://go-auth-v1.onrender.com/',
-    apiKey:'9f405f4d7fee7e618d52e6cb388c6b6a',
+    apiUrl: '',
+    apiKey: '',
 };
